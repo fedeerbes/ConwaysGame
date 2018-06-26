cb580387fd0ea535be1aef59fc389778236b9a1e
Esteban Erbes <fedeerbes@gmail.com>
2018-06-26 20:22:50 -0300

    refactor gameEngine to make it more readable

--- conwaysgame/src/components/dummy/grid/component.js
+++ conwaysgame/src/components/dummy/grid/component.js
@@ -5,22 +5,26 @@ import styles from "./styles.js";
 class DummyGrid extends Component<{}> {
 
   _renderGrid = () => {
-    let renderGrid = [],
-        grid = this.props.grid;
+    let renderGrid = [];
+    const grid = this.props.grid;
 
         if (grid.length !== 0) {
-          grid.map((column, index) => {
-            let columnArray = [];
-            column.map((cell, innerIndex) => {
-              columnArray.push(<View key={innerIndex} style={[{backgroundColor: cell ? "#FFFFFF" : "#000000"}, styles.cell]}></View>)
-            })
-  
+          for (let i = 0; i < grid.length; i += 1) {
+            //Go through the columns
+            const currentColumn = grid[i];
+            let renderColumn = [];
+            for (let j = 0; j < currentColumn.length; j += 1) {
+              //Go through the cells and push white views if the cell is alive and black otherwise
+              const cell = currentColumn[j];
+              renderColumn.push(<View key={j} style={[{backgroundColor: cell ? "#FFFFFF" : "#000000"}, styles.cell]}></View>)
+            }
+            //Push columns to the grid view
             renderGrid.push(
-              <View style={{flexDirection: "row"}} key={index}>
-                {columnArray}
+              <View style={{flexDirection: "row"}} key={i}>
+                {renderColumn}
               </View>
             )
-          })
+          }
         }
     return renderGrid;
   }
--- conwaysgame/src/utils/gameEngine/index.js
+++ conwaysgame/src/utils/gameEngine/index.js
@@ -25,57 +25,67 @@ class gameEngine {
     updatedGrid[1][3] = true;
     updatedGrid[2][1] = true;
     updatedGrid[2][2] = true;
-    
+
     return {grid: updatedGrid, population: this.getPopulation(updatedGrid)};
   }
 
   getPopulation = (grid) => {
     let population = 0;
-    grid.map((column) => {
-      column.map((cell) => {
-        if (cell) population += 1;
-      })
-    })
-
+    for (let i = 0; i < grid.length; i += 1) {
+      const columnArray = grid[i];
+      for (let j = 0; j < columnArray.length; j += 1) {
+        if (columnArray[j]) population += 1;
+      }
+    }
     return population;
   }
 
   stepForward = (grid) => {
-    let currentGrid = [...grid],
-        updatedGrid = [];
-    currentGrid.map((column, indexColumn) => {
-      let columnArray = [];
-      column.map((cell, indexCell) => {
-        let count = 0,
-            isAlive = cell;
+    //Get current grid a make a copy
+    const currentGrid = [...grid];
+    let updatedGrid = [];
+
+    for (let i = 0; i < currentGrid.length; i += 1) {
+      //Go through the columns
+      const currentColumn = currentGrid[i];
+      let updatedColumn = [];
+      for (let j = 0; j < currentColumn.length; j += 1) {
+        //Go through the cells
+        const cell = currentColumn[j];
+        let neighborCounting = 0,
+            cellStatus = currentColumn[j];
         
         //Ask about the state of the neighboring cell
-        if ((indexColumn - 1 >= 0) && (indexCell - 1 >= 0) && currentGrid[indexColumn - 1][indexCell - 1]) count += 1;
-        if ((indexColumn - 1 >= 0) && (indexCell >= 0) && currentGrid[indexColumn - 1][indexCell]) count += 1;
-        if ((indexColumn - 1 >= 0) && (indexCell + 1 >= 0) && currentGrid[indexColumn - 1][indexCell + 1]) count += 1;
-        if ((indexColumn >= 0) && (indexCell - 1 >= 0) && currentGrid[indexColumn][indexCell - 1]) count += 1;
-        if ((indexColumn >= 0) && (indexCell + 1 >= 0) && currentGrid[indexColumn][indexCell + 1]) count += 1;
-        if ((indexColumn + 1 >= 0) && (indexCell - 1 >= 0) && (indexColumn + 1 < column.length) && currentGrid[indexColumn + 1][indexCell - 1]) count += 1;
-        if ((indexColumn + 1 >= 0) && (indexCell >= 0) && (indexColumn + 1 < column.length) && currentGrid[indexColumn + 1][indexCell]) count += 1;
-        if ((indexColumn + 1 >= 0) && (indexCell + 1 >= 0) && (indexColumn + 1 < column.length) && currentGrid[indexColumn + 1][indexCell + 1]) count += 1;
+        if ((i - 1 >= 0) && (j - 1 >= 0) && currentGrid[i - 1][j - 1]) neighborCounting += 1;
+        if ((i - 1 >= 0) && (j >= 0) && currentGrid[i - 1][j]) neighborCounting += 1;
+        if ((i - 1 >= 0) && (j + 1 >= 0) && currentGrid[i - 1][j + 1]) neighborCounting += 1;
+        if ((i >= 0) && (j - 1 >= 0) && currentGrid[i][j - 1]) neighborCounting += 1;
+        if ((i >= 0) && (j + 1 >= 0) && currentGrid[i][j + 1]) neighborCounting += 1;
+        if ((i + 1 >= 0) && (j - 1 >= 0) && (i + 1 < currentColumn.length) && currentGrid[i + 1][j - 1]) neighborCounting += 1;
+        if ((i + 1 >= 0) && (j >= 0) && (i + 1 < currentColumn.length) && currentGrid[i + 1][j]) neighborCounting += 1;
+        if ((i + 1 >= 0) && (j + 1 >= 0) && (i + 1 < currentColumn.length) && currentGrid[i + 1][j + 1]) neighborCounting += 1;
         
+        //Establish rules of the game
         if (cell) {
           // Any live cell with fewer than two live neighbors dies, as if by under population.
-          if (count < 2) isAlive = false;
+          if (neighborCounting < 2) cellStatus = false;
 
           // Any live cell with two or three live neighbors lives on to the next generation.
-          if (count >= 2 && count < 4) isAlive = true;
+          if (neighborCounting >= 2 && neighborCounting < 4) cellStatus = true;
 
           // Any live cell with more than three live neighbors dies, as if by overpopulation.
-          if (count > 3) isAlive = false;
+          if (neighborCounting > 3) cellStatus = false;
         } else {
           // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-          if (count === 3) isAlive = true;
+          if (neighborCounting === 3) cellStatus = true;
         }
-        columnArray.push(isAlive)
-      })
-      updatedGrid.push(columnArray)
-    })
+        //Set new status of the current cell to the updated column
+        updatedColumn.push(cellStatus)
+      }
+      //Set new status of the current column to the updated grid
+      updatedGrid.push(updatedColumn)
+    }
+
     return {grid: updatedGrid, population: this.getPopulation(updatedGrid)};
   }
 }
